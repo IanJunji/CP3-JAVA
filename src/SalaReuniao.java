052eb183fb7991074420d3ba70d5ae9aa0bd6b86
Ian Junji <ianmatsushita@gmail.com>
2025-10-22 22:59:26 -0300

    init

---
+++ src/SalaReuniao.java
@@ -0,0 +1,5 @@
+public class SalaReuniao extends Espaco{
+    public SalaReuniao(String id, String nome, String localizacao, int capacidade){
+        super(id, nome, localizacao, capacidade);
+    }
+}
